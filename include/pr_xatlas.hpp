acfb77d5e7bde285f040befe0620f14166324c5a
Silverlan <silverlan@pragma-engine.com>
2025-11-18 20:40:24 +0100

    refactor: migrate library to C++20 modules

--- include/pr_xatlas.hpp
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: (c) 2020 Silverlan <opensource@pragma-engine.com>
-// SPDX-License-Identifier: MIT
-
-#ifndef __PR_XATLAS_HPP__
-#define __PR_XATLAS_HPP__
-
-#include <luasystem.h>
-
-#endif
